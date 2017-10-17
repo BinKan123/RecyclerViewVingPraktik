6bc903efa9b09b6b512137e395299776687cd5e7
KBINAN <bin.kan@thomascook.se>
2017-10-17 23:57:21 +0200

    facebook

--- app/src/main/java/com/example/kanbi/recyclerviewmvvm/minSida/EmailLogin.java
+++ app/src/main/java/com/example/kanbi/recyclerviewmvvm/minSida/EmailLogin.java
@@ -1,23 +1,24 @@
 package com.example.kanbi.recyclerviewmvvm.minSida;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import com.facebook.FacebookSdk;
 
 import com.example.kanbi.recyclerviewmvvm.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class EmailLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_email_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+
+
 
 
     }
