75af31b76dac0e32097457363cc51e906fbcf2fe
KBINAN <bin.kan@thomascook.se>
2017-10-18 23:56:07 +0200

    google

--- app/src/main/java/com/example/kanbi/recyclerviewmvvm/minSida/LoginActivity.java
+++ app/src/main/java/com/example/kanbi/recyclerviewmvvm/minSida/LoginActivity.java
@@ -1,16 +1,69 @@
 package com.example.kanbi.recyclerviewmvvm.minSida;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 
 import com.example.kanbi.recyclerviewmvvm.R;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
+
+import static com.example.kanbi.recyclerviewmvvm.R.drawable.minsida;
 
 public class LoginActivity extends AppCompatActivity {
+    //firebase
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    //facebook signin variables
+    private CallbackManager callbackManager;
+    private AccessToken facebookAccessToken;
+    private LoginButton facebookLogin;
+    private static final String TAG = "LoginActivity";
+
+    //google signin varialbles
+
+    private SignInButton googleLogin;
+    private static final int RC_SIGN_IN =1;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +72,191 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    startActivity(new Intent(LoginActivity.this, myActivity.class));
+                }
+            }
+        };
+        /*startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+                .setProviders(
+                        AuthUI.FACEBOOK_PROVIDER,
+                        AuthUI.EMAIL_PROVIDER,
+                        AuthUI.GOOGLE_PROVIDER
+                ).build(),1);*/
+        //facebook
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+        facebookLogin = (LoginButton) findViewById(R.id.loginWithFB);
+        initializeFacebookLogin();
+
+        //google
+
+
+        googleLogin = (SignInButton) findViewById(R.id.loginWithGoogle);
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(getApplicationContext())
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText(LoginActivity.this, "got an error", Toast.LENGTH_LONG).show();
+                    }
+                }).addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        googleLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                googleSignIn();
+            }
+        });
+    }
+
+
+
+    //face book
+    // check if the user is already loged in
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(mAuthListener);
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        if (currentUser!=null){
+            //change to nextActivity()
+            Intent intent = new Intent(this, myActivity.class);
+            startActivity(intent);
+        }
 
     }
 
+
+
+
+    //initialize facebook login button
+    private void initializeFacebookLogin() {
+        callbackManager = CallbackManager.Factory.create();
+        facebookLogin.setReadPermissions("email", "public_profile", "user_friends");
+        facebookLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+
+                Log.d(TAG, "facebook:onSuccess" + loginResult);
+                facebookAccessToken = loginResult.getAccessToken();
+                //handleFacebookAcessToken(facebookAccessToken);
+
+
+            }
+
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+
+            }
+        });
+    }
+
+
+
+        @Override
+        protected void onActivityResult(int requestCode,int resultCode, Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+            // Pass the activity result back to the Facebook SDK
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+
+
+
+
+
+
+
+
+
+    //Google signin
+    public void googleSignIn(){
+        Intent signInIntent= Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent,RC_SIGN_IN);
+    }
+
+
+   @Override
+   public void onActivityResultGoogle(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                //failed, inform user
+            }
+        }
+    }
+
+        private void firebaseAuthWithGoogle(GoogleSignInAccount acct){
+            Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+            AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+            firebaseAuth.signInWithCredential(credential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithCredential:success");
+                                FirebaseUser user = firebaseAuth.getCurrentUser();
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithCredential:failure", task.getException());
+                                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                               // updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+
+        }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/kanbi/recyclerviewmvvm/minSida/MinSidaFragment.java
+++ app/src/main/java/com/example/kanbi/recyclerviewmvvm/minSida/MinSidaFragment.java
@@ -1,5 +1,6 @@
 package com.example.kanbi.recyclerviewmvvm.minSida;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -8,8 +9,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.kanbi.recyclerviewmvvm.R;
+import com.example.kanbi.recyclerviewmvvm.inspirationBooking.inspirationHotel;
 
 /**
  * Created by kanbi on 10/10/2017.
@@ -21,6 +24,17 @@ public class MinSidaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.minsida_fragment,container,false);
 
+        Button loginBtn = (Button) view.findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity().getApplication(), LoginActivity.class);
+                getActivity().startActivity(intent);
+            }
+
+        });
 
         FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener()
